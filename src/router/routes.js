11d4a9f681fea1ca9a8224dbe8a8b9db112bab47
Luan <luanchristian.rochadocarmo@gmail.com>
2023-09-17 20:28:43 -0300

    fix: correção de bug

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,16 +4,17 @@ import { db } from "../data/db.js";
 export const appRoutes = Router()
 
 appRoutes.get('/', (req, res) => {
-  res.send('Bem-vindo à minha aplicação Node.js com MySQL!');
+  res.render('index');
 });
 
 appRoutes.get('/movies', (req, res) => {
-  db.query('SELECT * FROM `movies`', function (err, results, fields) {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Erro ao buscar dados do banco de dados' });
-    }
-    res.json(results); // Envia os resultados como resposta no formato JSON
+  connection.query('SELECT * FROM `movies`', function (err, results, fields) {
+      if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Erro ao buscar dados do banco de dados' });
+      }
+      res.render('movies', {movies: results})
+      // res.json(results); // Envia os resultados como resposta no formato JSON
   });
 });
 
@@ -22,40 +23,39 @@ appRoutes.get('/movies/:id', (req, res) => {
   const query = 'SELECT * FROM `movies` WHERE cod = ?';
 
   db.query(query, [id], function (err, results, fields) {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Erro ao buscar dados do banco de dados' });
-    }
+      if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Erro ao buscar dados do banco de dados' });
+      }
 
-    if (results.length === 0) {
-      return res.status(404).json({ error: 'Filme não encontrado' });
-    }
+      if (results.length === 0) {
+          return res.status(404).json({ error: 'Filme não encontrado' });
+      }
 
-    res.json(results[0]); // Envia o primeiro resultado como resposta no formato JSON
+      res.json(results[0]); // Envia o primeiro resultado como resposta no formato JSON
   });
 });
 
 // Rota para inserir um novo filme
 appRoutes.post('/new', async (req, res) => {
   try {
-    const { titulo, sinopse, duracao, imagem, dataLancamento } = req.body;
-    if (!titulo || !sinopse || !duracao || !imagem || !dataLancamento) {
-      return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
-    }
+      const { titulo, sinopse, duracao, imagem, dataLancamento } = req.body;
+      if (!titulo || !sinopse || !duracao || !imagem || !dataLancamento) {
+          return res.status(400).json({ error: 'Todos os campos são obrigatórios' });
+      }
 
-    // Decodifica a imagem base64 para binário
-    const imagemBinario = Buffer.from(imagem, 'base64');
+      // Decodifica a imagem base64 para binário
+      const imagemBinario = Buffer.from(imagem, 'base64');
 
-    const query = 'INSERT INTO `movies` (titulo, sinopse, duracao, imagem, dataLancamento) VALUES (?, ?, ?, ?, ?)';
-    
-    const values = [titulo, sinopse, duracao, imagemBinario, dataLancamento];
+      const query = 'INSERT INTO `movies` (titulo, sinopse, duracao, imagem, dataLancamento) VALUES (?, ?, ?, ?, ?)';
+      const values = [titulo, sinopse, duracao, imagemBinario, dataLancamento];
 
-    const [result] = await db.query(query, values);
+      const [result] = db.query(query, values);
 
-    res.status(201).json({ message: 'Filme inserido com sucesso' });
-    
+      res.status(201).json({ message: 'Filme inserido com sucesso' });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Erro ao inserir dados no banco de dados' });
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao inserir dados no banco de dados' });
   }
 });
+
